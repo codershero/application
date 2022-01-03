deb41b0aad0b2fa2fc8b09900356c55db17f2a16
codershero <codershero5@gmail.com>
2022-01-03 06:32:11 +0600

    1st

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import Vector from '../public/img/Frame.svg'
+import Heart from '../public/img/Shape.svg'
+import Ezgif from '../public/img/ezgif.svg'
+import Day from '../public/img/day.svg'
+import Bid from '../public/img/bid.svg'
+import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,43 +16,31 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.card}>
+          <div className={styles.topContentsBox}>
+            <div className={styles.contents}>
+              <Image src={Vector} height={40} width={100} alt='icon' />
+            </div>
+            <div className={styles.iconTop}>
+              <Image src={Heart} height={40} width={100} alt='icon' />
+              <p className={styles.heartText}>27</p>
+            </div>
+          </div>
+          <div className={styles.imgBox}>
+            <Image src={Ezgif} height={350} width={350} alt='icon' />
+          </div>
+          <div className={styles.bottomText}>
+            <small>DenzeWashingto</small>
+            <h3>Nike Air Jordan 1 OG</h3>
+          </div>
+          <div className={styles.bottomFlex}>
+            <div>
+              <Image src={Day} height={50} width={150} alt='icon' />
+            </div>
+            <div>
+              <Image src={Bid} height={50} width={150} alt='icon' />
+            </div>
+          </div>
         </div>
       </main>
 
